513b53979afd8ca278371c015dbd1a610db1187a
Maru665 <71581173+Maru665@users.noreply.github.com>
2021-03-04 23:51:05 +0900

    4章テスト課題
    Java４章

--- JavaStudy/Check4/src/service/EmployeeService.java
+++ JavaStudy/Check4/src/service/EmployeeService.java
@@ -35,7 +35,7 @@ public class EmployeeService {
 
   // 問③ 入力されたIDとPassWordをキーにして、検索するSELECT文
  /** ・SQL SELECT文 */
- private static final String SQL_SELECT = "SELECT id,password,name,comment,login_time FROM employee_table WHERE id=?,password=?";
+ private static final String SQL_SELECT = "SELECT * FROM employee_table WHERE id=? AND password=?";
 
  EmployeeBean employeeDate = null;
 
@@ -68,7 +68,7 @@ public class EmployeeService {
  preparedStatement = connection.prepareStatement(SQL_UPDATE);
   // 問④ preparedStatementを使って、一番目のindexに今の時間をセットしてください。2番目のindexにIDをセットしてください。
  preparedStatement.setString(1,login_time);
- preparedStatement.setString(2, "id");
+ preparedStatement.setString(2, id);
   // 問⑤ UPDATEを実行する文を記述
  preparedStatement.executeUpdate();
 
@@ -79,7 +79,7 @@ public class EmployeeService {
  preparedStatement = connection.prepareStatement(SQL_SELECT);
   //問⑥ 一番目のindexにIDをセットしてください。2番目のindexにPASSWORDをセット。
  preparedStatement.setString(1, id);
- preparedStatement.setString(2,"admin");
+ preparedStatement.setString(2,password);
   // SQLを実行。実行した結果をresultSetに格納。
  resultSet = preparedStatement.executeQuery();
 
